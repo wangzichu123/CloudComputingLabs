411f420a786111ec7f52a2a539edc93e6140e197
gaobanzou <906137480@qq.com>
2020-03-31 18:07:39 -0700

    have a try

---
+++ Lab1/src/try2/线程测试.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#define MAXSTACK 100
+int stack[MAXSTACK][2];
+int size=0;
+sem_t sem;
+
+
+void *ReadData1(void*){
+	FILE *fp=fopen("1.txt","r");
+	while(!feof(fp)){
+		fscanf(fp,"%d %d",&stack[size][0],&stack[size][1]);
+		sem_post(&sem);
+		++size;
+	}
+	fclose(fp);
+}
+
+
+void *ReadData2(void*){
+	FILE *fp=fopen("2.txt","r");
+	while(!feof(fp)){
+		fscanf(fp,"%d %d",&stack[size][0],&stack[size][1]);
+		sem_post(&sem);
+		++size;
+	}
+	fclose(fp);
+}
+
+void *HandleData1(void* arg){
+	while(1){
+		sem_wait(&sem);
+		printf("≤‚ ‘1");
+		--size;
+	}
+}
+
+void *HandleData2(void* arg){
+	while(1){
+		sem_wait(&sem);
+		printf("≤‚ ‘2");
+		--size;
+	}
+}
+
+int main(void){
+	pthread_t t1,t2,t3,t4;
+	sem_init(&sem,0,0);
+	pthread_create(&t1,NULL,HandleData1,NULL);
+	pthread_create(&t2,NULL,HandleData2,NULL);
+	pthread_create(&t3,NULL,ReadData1,NULL);
+	pthread_create(&t4,NULL,ReadData2,NULL);
+	pthread_join(t1,NULL);
+}
